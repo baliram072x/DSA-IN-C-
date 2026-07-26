998c8c012f83e6fb05ae79b07bc14ece1835e572
baliram072x <baliramrathod0008@gmail.com>
2026-07-26 23:13:02 +0530

    update

--- SECONDLARGEST/Index.c
+++ SECONDLARGEST/Index.c
@@ -1,29 +1,69 @@
-//Q.  Write a program to find all duplicate elements present in an array.
-#include<stdio.h>
-int main(){
-    int arr[5],i,j;
+// //Q.  Write a program to find all duplicate elements present in an array.
+// #include<stdio.h>
+// int main(){
+//     int arr[5],i,j;
 
-    printf("enter array element :");
-    for(i=0; i<5; i++){
+//     printf("enter array element :");
+//     for(i=0; i<5; i++){
+//         scanf("%d", &arr[i]);
+//     }
+
+//     printf("Array");
+//     for(i=0; i<5; i++){
+//         printf(" %d ",arr[i]);
+//     }
+
+//     for(i=0; i<5; i++){
+        
+//         for(j =0; j<5; j++){
+//        if (arr[i]<=arr[j+1] && arr[i]>=arr[j+2])
+//        {
+//         arr[i] = i+1;
+//        }
+//        printf("%d" , arr[i]);
+       
+//     }
+
+
+//     }
+// }
+
+
+
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    printf("Array");
-    for(i=0; i<5; i++){
-        printf(" %d ",arr[i]);
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
     }
 
-    for(i=0; i<5; i++){
-        
-        for(j =0; j<5; j++){
-       if (arr[i]<=arr[j+1] && arr[i]>=arr[j+2])
-       {
-        arr[i] = i+1;
-       }
-       printf("%d" , arr[i]);
-       
+    if(secondLargest == INT_MIN) {
+        printf("Second largest element does not exist.");
+    } else {
+        printf("Second Largest = %d", secondLargest);
     }
 
-
-    }
+    return 0;
 }
